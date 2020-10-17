6f512fa49128a4ec1ba65c6033eb619b2d36598b
leticiakanegae <lckanegae@gmail.com>
2020-10-17 14:13:56 -0300

    Revert

---
+++ app/models/cocktail.rb
@@ -0,0 +1,7 @@
+class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+end
